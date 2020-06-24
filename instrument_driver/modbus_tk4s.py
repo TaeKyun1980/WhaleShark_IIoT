8892d0248578d1ba29762157744b13f5535568f6
prismdata-dc <prismdata@add4s.com>
2020-06-24 10:46:01 +0900

    autonics tk4s modbus

---
+++ instrument_driver/modbus_tk4s.py
@@ -0,0 +1,38 @@
+import minimalmodbus
+import math
+
+class autonics_pid_tk4s:
+
+    ##init comport
+    def __init__(self):
+        self.instrument = minimalmodbus.Instrument('COM3', 1, minimalmodbus.MODE_RTU) # port name, slave address (in decimal), type
+        self.instrument.serial.baudrate = 19200
+        self.instrument.serial.bytesize = 8
+        self.instrument.serial.parity = minimalmodbus.serial.PARITY_NONE
+        self.instrument.serial.stopbits = 2
+        self.instrument.serial.timeout = 1.5
+        self.instrument.debug = True
+        self.instrument.mode = minimalmodbus.MODE_RTU
+    def __init__(self, mode):
+        if mode == 'emulation':
+            self.mode = mode
+            pass
+
+    ##scanning pv
+    def scan_pv(self):
+        try:
+            if self.mode != 'emulation':
+                pv_address = 1000
+                prevision_address = 1001
+                print('pv_address:', pv_address,'prevision_address:', prevision_address)
+                pv = self.instrument.read_register(pv_address, 0, functioncode=int('0x04', 16))
+                precision = self.instrument.read_register(prevision_address, 0, functioncode=int('0x04', 16))
+                precision = math.pow(10, precision)
+                pv = float(pv) / precision
+                return pv, precision
+            else:
+                return 100, 10
+        except Exception as e:
+            print(e)
+
+
