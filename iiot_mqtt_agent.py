e0dbcd3aad69c84ba7b3cf38d0b165a00744f9e4
prismdata-dc <prismdata@add4s.com>
2020-10-10 19:36:15 +0900

    clean code lvl by flake8

--- iiot_mqtt_agent.py
+++ iiot_mqtt_agent.py
@@ -52,7 +52,9 @@ def connect_influxdb(host, port, id, pwd, db):
 def get_messagequeue(address, port):
     '''
     If you don't have rabbitmq, you can use docker.
-    docker run -d --hostname whaleshark --name whaleshark-rabbit -p 5672:5672 -p 8080:15672 -e RABBITMQ_DEFAULT_USER=whaleshark -e RABBITMQ_DEFAULT_PASS=whaleshark rabbitmq:3-management
+    docker run -d --hostname whaleshark --name whaleshark-rabbit \
+    -p 5672:5672 -p 8080:15672 -e RABBITMQ_DEFAULT_USER=whaleshark \
+    -e RABBITMQ_DEFAULT_PASS=whaleshark rabbitmq:3-management
 
     get message queue connector (rabbit mq) with address, port
     :param address: rabbit mq server ip
