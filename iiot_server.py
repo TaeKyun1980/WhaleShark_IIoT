977fa0bfbadff5af619cdec54f72b3aae9282afb
prismdata-dc <prismdata@add4s.com>
2020-10-10 19:27:17 +0900

    clean code lvl by flake8

--- iiot_server.py
+++ iiot_server.py
@@ -108,7 +108,9 @@ class tcp_server:
     def get_messagequeue(self, address, port):
         '''
         If you don't have rabbitmq, you can use docker.
-        docker run -d --hostname whaleshark --name whaleshark-rabbit -p 5672:5672 -p 8080:15672 -e RABBITMQ_DEFAULT_USER=whaleshark -e RABBITMQ_DEFAULT_PASS=whaleshark rabbitmq:3-management
+        docker run -d --hostname whaleshark --name whaleshark-rabbit -p 5672:5672 \
+        -p 8080:15672 -e RABBITMQ_DEFAULT_USER=whaleshark -e \
+        RABBITMQ_DEFAULT_PASS=whaleshark rabbitmq:3-management
 
         get message queue connector (rabbit mq) with address, port
         :param address: rabbit mq server ip
