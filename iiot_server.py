f46b9550d6f6d3a22220bb8896bb0745c5018dba
prismdata-dc <prismdata@add4s.com>
2020-10-10 19:24:29 +0900

    clean code lvl by flake8

--- iiot_server.py
+++ iiot_server.py
@@ -85,18 +85,18 @@ class tcp_server:
             facilities_dict = redis_con.get('facilities_info')
 
             if facilities_dict is None:
-                facilities_dict = {'TS0001':{
-                    '0001':'TS_VOLT1_(RS)',
-                    '0002':'TS_VOLT1_(ST)',
-                    '0003':'TS_VOLT1_(RT)',
-                    '0004':'TS_AMP1_(R)',
-                    '0005':'TS_AMP1_(S)',
-                    '0006':'TS_AMP1_(T)',
-                    '0007':'INNER_PRESS',
-                    '0008':'PUMP_PRESS',
-                    '0009':'TEMPERATURE1(PV)',
-	                '0010':'TEMPERATURE1(SV)',
-                    '0011':'OVER_TEMP'}
+                facilities_dict = {'TS0001': {
+                    '0001': 'TS_VOLT1_(RS)',
+                    '0002': 'TS_VOLT1_(ST)',
+                    '0003': 'TS_VOLT1_(RT)',
+                    '0004': 'TS_AMP1_(R)',
+                    '0005': 'TS_AMP1_(S)',
+                    '0006': 'TS_AMP1_(T)',
+                    '0007': 'INNER_PRESS',
+                    '0008': 'PUMP_PRESS',
+                    '0009': 'TEMPERATURE1(PV)',
+	                '0010': 'TEMPERATURE1(SV)',
+                    '0011': 'OVER_TEMP'}
                     }
                 redis_con.set('facilities_info',json.dumps(facilities_dict))
 
