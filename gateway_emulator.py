7f5059243542dc68aa0df272122de6e8b7b2387b
prismdata-dc <prismdata@add4s.com>
2020-10-18 12:22:24 +0900

    Update gateway emulation channel, sample data

--- gateway_emulator.py
+++ gateway_emulator.py
@@ -32,13 +32,13 @@ def convert(list):
 
 
 if __name__ == '__main__':
-    csv_test = pd.read_csv('TS_sample.csv')
+    csv_test = pd.read_csv('sample_data/TS_small.csv')
     Emulation_Data = csv_test.drop(['name', 'time', 'INNER_PRESS_1', 'OVER_TEMP_1', 'PUMP_PRESS_1', 'TEMPERATURE1_1',
                                     'TS_AMP1_(R)_1', 'TS_AMP1_(S)_1', 'TS_AMP1_(T)_1', 'TS_VOLT1_(RS)_1',
                                     'TS_VOLT1_(RT)_1', 'TS_VOLT1_(ST)_1'], axis=1)
     Emulation_Data.columns.to_list()
     Emulation_Data_json = Emulation_Data.to_dict(orient='records')
-    column_list = ['TS_AMP1_(R)', 'TS_AMP1_(S)', 'TS_AMP1_(T)']
+    column_list = ['TS_AMP1_(R)', 'TS_AMP1_(S)', 'TS_AMP1_(T)','INNER_PRESS','PUMP_PRESS']
     # Sample packet (2, 0, 0, 0, 0, 84, 83, 0, 1, 0, 9, 80, 86, 0, 0, 1, 74, 1, 3)
     # Sample packet2 (2, 0, 0, 0, 0, 84, 83, 0, 101, 0, 1, 80, 86, 0, 0, 0, 175, 1, 3)
     # facility_id 설비명 / 설비 ID, sensor_code : 센서에 대한 설명(iiot_server의 facilities_dict 참조), pv : 현재 센서 값
@@ -55,7 +55,7 @@ if __name__ == '__main__':
                      'OVER_TEMP': 11}
     
     for i, emulation_data in enumerate(Emulation_Data_json):
-        for column in column_list[0:1]:
+        for column in column_list:
             print(i, column, 'code{:04d}'.format(sensor_id_map[column]), 'PV', int(emulation_data[column] / 100))
             packet = make_packet(facility_id='TS0001', sensor_code='{:04d}'.format(sensor_id_map[column]),
                                  pv=int(emulation_data[column] / 1000))
