e90af9d1e038cda407c1a55869760b0ac3352967
jook119 <jook@add4s.com>
2020-10-21 14:36:05 +0900

    clean code by PEP8

--- iiot_mqtt_agent.py
+++ iiot_mqtt_agent.py
@@ -4,17 +4,12 @@ import json
 import pika
 import sys
 import redis
-<<<<<<< HEAD
-=======
-from datetime import timedelta
-import datetime
->>>>>>> upstream/master
 from influxdb import InfluxDBClient
 import time
+from WhaleShark_IIoT import mongo_manager
 
-import mongo_manager
-
-logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',stream=sys.stdout, level=logging.DEBUG, datefmt='%Y-%m-%d %H:%M:%S')
+logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',
+                    stream=sys.stdout, level=logging.DEBUG, datefmt='%Y-%m-%d %H:%M:%S')
 logging.getLogger("pika").propagate = False
 
 mongo_mgr = mongo_manager.MongoMgr()
@@ -47,7 +42,7 @@ def connect_redis(host, port):
     return redis_obj
 
 
-def connect_influxdb(host, port, id, pwd, db):
+def con_influxdb(host, port, id, pwd, db):
     """
     :param host: InfluxDB access host ip
     :param port: InfluxDB access port
@@ -87,30 +82,30 @@ def get_messagequeue(address, port):
 
 
 influxdb_client = None
+
+
 def callback_mqreceive(ch, method, properties, body):
     body = body.decode('utf-8')
     facility_msg_json = json.loads(body)
     table_name = list(facility_msg_json.keys())[0]
     fields = {}
     tags = {}
-    logging.debug('mqtt body:'+ str(facility_msg_json))
+    logging.debug('mqtt body:' + str(facility_msg_json))
     me_timestamp = time.time()
     for key in facility_msg_json[table_name].keys():
-        #tags[key] = float(facility_msg_json[table_name][key])
         if key != 'pub_time':
             logging.debug('config key:'+key + 'value:'+str(facility_msg_json[table_name][key]))
             fields[key] = float(facility_msg_json[table_name][key])
        
     pub_time = facility_msg_json[table_name]['pub_time']
-    day = pub_time.split(' ')[0]#.replace('T','')
-    pub_doc = mongo_mgr.document_bykey('facility', table_name, {'DAY':day})
+    day = pub_time.split(' ')[0]
+    pub_doc = mongo_mgr.document_bykey('facility', table_name, {'DAY': day})
     if pub_doc is not None:
         mongo_mgr.document_upsert('facility', table_name, day, pub_time, status='CHECK')
     
     fields['me_time'] = me_timestamp
     influx_json = [{
         'measurement': table_name,
-        # 'tags':tags,
         'fields': fields
     }]
     try:
@@ -127,19 +122,19 @@ def config_facility_desc(redis_con):
 
     if facilities_dict is None:
         facilities_dict = {'TS0001': {
-            '0001':'TS_VOLT1_(RS)',
-            '0002':'TS_VOLT1_(ST)',
-            '0003':'TS_VOLT1_(RT)',
-            '0004':'TS_AMP1_(R)',
-            '0005':'TS_AMP1_(S)',
-            '0006':'TS_AMP1_(T)',
-            '0007':'INNER_PRESS',
-            '0008':'PUMP_PRESS',
-            '0009':'TEMPERATURE1(PV)',
-            '0010':'TEMPERATURE1(SV)',
-            '0011':'OVER_TEMP'}
+            '0001': 'TS_VOLT1_(RS)',
+            '0002': 'TS_VOLT1_(ST)',
+            '0003': 'TS_VOLT1_(RT)',
+            '0004': 'TS_AMP1_(R)',
+            '0005': 'TS_AMP1_(S)',
+            '0006': 'TS_AMP1_(T)',
+            '0007': 'INNER_PRESS',
+            '0008': 'PUMP_PRESS',
+            '0009': 'TEMPERATURE1(PV)',
+            '0010': 'TEMPERATURE1(SV)',
+            '0011': 'OVER_TEMP'}
         }
-        redis_con.set('facilities_info',json.dumps(facilities_dict))
+        redis_con.set('facilities_info', json.dumps(facilities_dict))
 
 
 if __name__ == '__main__':
@@ -151,19 +146,19 @@ if __name__ == '__main__':
         redis_host = config_obj['iiot_server']['redis_server']['ip_address']
         redis_port = config_obj['iiot_server']['redis_server']['port']
         
-        influxdb_host = config_obj['iiot_server']['influxdb']['ip_address']
-        influxdb_port = config_obj['iiot_server']['influxdb']['port']
+        influx_host = config_obj['iiot_server']['influxdb']['ip_address']
+        influx_port = config_obj['iiot_server']['influxdb']['port']
         
-        influxdb_id = config_obj['iiot_server']['influxdb']['id']
-        influxdb_pwd = config_obj['iiot_server']['influxdb']['pwd']
-        influxdb_db = config_obj['iiot_server']['influxdb']['db']
+        influx_id = config_obj['iiot_server']['influxdb']['id']
+        influx_pwd = config_obj['iiot_server']['influxdb']['pwd']
+        influx_db = config_obj['iiot_server']['influxdb']['db']
     
-    influxdb_client = connect_influxdb(host=influxdb_host, port=influxdb_port, id=influxdb_id, pwd=influxdb_pwd, db=influxdb_db)
-    if influxdb_client == None:
+    influxdb_client = con_influxdb(host=influx_host, port=influx_port, id=influx_id, pwd=influx_pwd, db=influx_db)
+    if influxdb_client is None:
         logging.error('influxdb configuration fail')
         
     mq_channel = get_messagequeue(address=rabbitmq_host, port=rabbitmq_port)
-    if mq_channel == None:
+    if mq_channel is None:
         logging.error('rabbitmq configuration fail')
     
     redis_con = connect_redis(redis_host, redis_port)
